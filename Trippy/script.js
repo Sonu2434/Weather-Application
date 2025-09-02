96d9c72e2fc9907a544fdaad524fa8ce3f7b30dc
Sonu Sanju <sonus24347@gmail.com>
2025-09-02 23:10:56 +0530

    Add files via upload
    ☀️🌧️ A simple and interactive Weather App that provides real-time weather updates using API integration.

---
+++ Trippy/script.js
@@ -0,0 +1,20 @@
+
+// ----------API To get Locations---------------
+
+// const url = 'https://google-search74.p.rapidapi.com/?query=Tourism%20places%20in%20giridih%2Cjharkhand&limit=10&related_keywords=true';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '91492ec5fcmsh50e882349e886b1p1d21c6jsn232ddcf70475',
+// 		'x-rapidapi-host': 'google-search74.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
